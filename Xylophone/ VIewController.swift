c7a07566f21e3bbc482adf35c47a5c9ece8c5caf
pmedas <pedro.medas@gmail.com>
2018-01-07 19:36:21 +0000

    use of do try catch

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 import AudioToolbox
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +23,25 @@ class ViewController: UIViewController{
     @IBAction func notePressed(_ sender: UIButton) {
         
         let note: String = "note" + String(sender.tag)
-        if let soundURL = Bundle.main.url(forResource: note, withExtension: "wav") {
-            var mySound: SystemSoundID = 0
-            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
-            
-            AudioServicesPlaySystemSound(mySound)
+
+        let soundURL = Bundle.main.url(forResource: note, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
         }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+
+//        let note: String = "note" + String(sender.tag)
+//        if let soundURL = Bundle.main.url(forResource: note, withExtension: "wav") {
+//            var mySound: SystemSoundID = 0
+//            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+//
+//            AudioServicesPlaySystemSound(mySound)
+//        }
     }
     
 

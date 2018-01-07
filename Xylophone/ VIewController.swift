0e17196dc5987e3078ca485b20c6a30cf739892c
pmedas <pedro.medas@gmail.com>
2018-01-07 20:11:07 +0000

    change global variables by local variables

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -13,7 +13,6 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayer: AVAudioPlayer!
-    var selectedSoundFileName : String = ""
     var soundArray: Array = ["note1","note2","note3","note4","note5","note6","note7"]
     
     override func viewDidLoad() {
@@ -24,17 +23,13 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        selectedSoundFileName = soundArray[sender.tag - 1]
-        
-        print(selectedSoundFileName)
-        
-        playSound()
+        playSound(soundFileName: soundArray[sender.tag - 1])
 
     }
     
-    func playSound() {
-                
-        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+    func playSound(soundFileName : String) {
+        
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)

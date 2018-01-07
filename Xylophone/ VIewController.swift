d07402c39aa12d265a7296201afb525dbe17a2a9
pmedas <pedro.medas@gmail.com>
2018-01-07 20:03:10 +0000

    final

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -13,6 +13,7 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayer: AVAudioPlayer!
+    var selectedSoundFileName : String = ""
     var soundArray: Array = ["note1","note2","note3","note4","note5","note6","note7"]
     
     override func viewDidLoad() {
